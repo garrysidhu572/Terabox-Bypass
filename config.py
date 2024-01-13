729bf37cc42c96bc80492a135d00b306072848b9
Garry <115072157+garrysidhu572@users.noreply.github.com>
2024-01-13 17:10:51 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,9 +10,9 @@ logger = logging.getLogger(__name__)
 my_cookie = os.environ.get("MY_COOKIES")
 my_headers = os.environ.get("MY_HEADERS")
 session_string = os.environ.get("session_string")
-allowed_groups = os.environ.get("allowed_groups") or "-123232ZCVZB"  # added random group id to avoid NoneType error
-# allowed_groups = ["-123232ZCVZB"] or ["121222xxx", "123456xxx"]
-owner_id = os.environ.get("owner_id") or ""
+allowed_groups = os.environ.get("allowed_groups") or "-1002074421298"  # added random group id to avoid NoneType error
+# allowed_groups = ["-1002074421298"] or ["-1002074421298", "-1002074421298"]
+owner_id = os.environ.get("owner_id") or "1718738592"
 
 try:
     my_cookie = eval(my_cookie)
